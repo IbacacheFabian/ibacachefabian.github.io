f3c20f48f8606207ae797b37bbc81551c3721cc2
Fabien Antoine <fmunozibacache@gmail.com>
2025-08-02 05:59:23 -0400

    Update site info

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -38,42 +38,42 @@ export const portafolioData = [
 		averageBrightness: 0.1,
 	},
 	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
+		imgSrc: 'https://s3-figma-hubfile-images-production.figma.com/hub/file/carousel/img/35d8ca6ac2bdfd0e466436c14d3c3f101cf0c11f',
 		title: 'Trekking APP',
 		skills: ['React', 'CSS'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+			'Desarrollo en fase de Design Thinking, pendiente MVP.',
 		demoURL: '',
 		repoURL: '',
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: 'https://www.corse.mx/cdn/shop/files/tienda_lenceria-lenceria-hot_sale-ofertas-mexico-lenceria_seyx-encaje-corse_mx-corse_lenceria_1200_x_560_px_13.png?v=1712270274&width=1500',
-		title: 'Pendiente',
+		imgSrc: 'https://raw.githubusercontent.com/IbacacheFabian/ibacachefabian.github.io/refs/heads/main/public/img/Captura%20de%20Pantalla%202025-08-02%20a%20la(s)%2005.50.30.png',
+		title: 'Photography',
+		skills: ['React', 'CSS'],
+		descripcion:
+			'Galería de imagenes en sesiones en vivo de fotografía deportiva',
+		demoURL: 'https://www.flickr.com/photos/mastercaiser/albums/',
+		repoURL: '',
+		anim: 'fade-left',
+	},
+	{
+		imgSrc: 'https://raw.githubusercontent.com/IbacacheFabian/ibacachefabian.github.io/refs/heads/main/public/img/Captura%20de%20Pantalla%202025-08-02%20a%20la(s)%2005.49.46.png',
+		title: '3D design',
 		skills: ['React', 'CSS'],
 		descripcion:
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
+		demoURL: 'https://cults3d.com/@fmunozibacache',
 		repoURL: '',
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
-		title: 'Pendiente',
-		skills: ['React', 'CSS'],
+		imgSrc: 'https://raw.githubusercontent.com/IbacacheFabian/ibacachefabian.github.io/refs/heads/main/public/img/Captura%20de%20Pantalla%202025-08-02%20a%20la(s)%2005.35.14.png',
+		title: 'Implementación Jira',
+		skills: ['Sass', 'Astro'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-left',
-	},
-	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
-		title: 'Pendiente',
-		skills: ['React', 'StyledComponents'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
+			'Video Tutorial realizado en Bootcamp Product Owner como implementar Jira al sistema de trabajo.',
+		demoURL: 'https://www.linkedin.com/posts/fabian-ibacache_jira-activity-7344191180810715136-nz97?utm_source=share&utm_medium=member_desktop&rcm=ACoAABaSx-MBHuJdWA5eNZyt-PcSDJD3IqQ1Jl0',
 		repoURL: '',
 		anim: 'fade-right',
 	}
